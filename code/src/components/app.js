bf36280cbd8bec10294c6679e2437278b34e62da
Freja Busby <frejabusby@gmail.com>
2018-04-18 10:02:35 +0200

    First list

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -1,11 +1,29 @@
 import React from "react"
+import Item from "./item"
 
 class App extends React.Component {
+constructor(props){
+super(props)
+this.state = {
+  items: [
+    {id:1, done: false, text:"Bike to work"},
+    {id:2, done: false, text:"Eat organic"}
+  ]
+}
+}
+
 
   render() {
+  
     return (
       <div>
-        Find me in src/app.js!
+      {this.state.items.map(listItem => (
+      <Item key={listItem.id}
+        text={listItem.text}
+       />
+      ))}
+
+
       </div>
     )
   }
---
+++ code/src/components/item.js
@@ -0,0 +1,38 @@
+import React from "react"
+
+class Item extends React.Component {
+render() {
+
+return(
+
+  <div>
+<form>
+  <label>
+<input type="checkbox" />
+{this.props.text}
+
+</label>
+
+
+
+
+
+
+
+
+
+
+</form>
+
+
+  </div>
+
+)
+
+
+}
+
+
+}
+
+export default Item
